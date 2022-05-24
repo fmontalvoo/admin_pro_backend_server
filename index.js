509add3d71affd88e93a014a915986c8cb7c8805
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-05-24 11:25:51 -0500

    Configuracion para CORS.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 
 const { connection } = require('./database/config');
 
@@ -7,6 +8,11 @@ const app = express();
 connection();
 
 const port = process.env.PORT || 3000;
+
+// Cross-origin
+app.use(cors());
+// Serializacion
+app.use(express.json());
 
 app.use('/', (req, res) => {
     res.status(200).json({ msg: 'Hello World' });
