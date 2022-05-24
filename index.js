8f77e3e6ac722bab1a61677e50eb1fae08233807
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-05-24 10:39:36 -0500

    Configuracion de primera ruta.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
+app.use('/', (req, res) => {
+    res.status(200).json({ msg: 'Hello World' });
+});
+
 app.listen(port, () => {
     console.info(`Server is running on port ${port}`);
 });
