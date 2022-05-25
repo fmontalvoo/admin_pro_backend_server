33fc9e50035e75ba80546c320982615e6062f7f0
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-05-25 10:57:21 -0500

    Se retira el password del objeto de respuesta del usuario.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- models/usuario.model.js
+++ models/usuario.model.js
@@ -29,7 +29,7 @@ const UserSchema = new Schema({
 });
 
 UserSchema.method('toJSON', function () {
-    const { __v, _id, ...user } = this.toObject();
+    const { __v, _id, password, ...user } = this.toObject();
     user.uid = _id;
     return user;
 });
