66fadd58934f63b7cd9c9b31b3e68c07d7f51b3f
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-05-26 13:52:42 -0500

    Se genera un API para hacer busquedas por nombre en todas las colecciones de la base de datos.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- controllers/busqueda.controller.js
+++ controllers/busqueda.controller.js
@@ -1,9 +1,24 @@
+const Doctor = require('../models/doctor.model');
+const Usuario = require('../models/usuario.model');
+const Hospital = require('../models/hospital.model');
+
 const search = async (req, res) => {
     try {
         const query = req.query.q || '';
-        return res.status(200).json({
-            query
-        });
+        const regex = new RegExp(query, 'i');
+
+        await Promise.all([
+            Doctor.find({ name: regex }),
+            Usuario.find({ name: regex }),
+            Hospital.find({ name: regex })
+        ])
+            .then(
+                ([doctores, usuarios, hospitales]) => res.status(200).json({ doctores, usuarios, hospitales })
+            )
+            .catch(
+                error => res.status(500).json({ message: error.message })
+            );
+
     } catch (error) {
         return res.status(500).json({
             message: error.message
