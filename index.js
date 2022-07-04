f63bdea5adae7a47105fcd6a14eaecd0d5b388f4
Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>
2022-07-04 12:09:02 -0500

    Se agrega configuracion para servir SPA en produccion.
    Signed-off-by: Frank Montalvo Ochoa <fmontalvoo@ups.edu.ec>

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 require('dotenv').config();
+const path = require('path');
+
 const express = require('express');
 const cors = require('cors');
 
@@ -8,7 +10,7 @@ const app = express();
 connection();
 
 const port = process.env.PORT || 3000;
-const path = '/api/v1';
+const version_path = '/api/v1';
 
 // Cross-origin
 app.use(cors());
@@ -18,12 +20,17 @@ app.use(express.json());
 app.use(express.static('public'));
 
 // Rutas
-app.use(`${path}/auth`, require('./routes/auth.routes'));
-app.use(`${path}/search`, require('./routes/busqueda.routes'));
-app.use(`${path}/uploads`, require('./routes/uploads.routes'));
-app.use(`${path}/doctores`, require('./routes/doctor.routes'));
-app.use(`${path}/usuarios`, require('./routes/usuario.routes'));
-app.use(`${path}/hospitales`, require('./routes/hospital.routes'));
+app.use(`${version_path}/auth`, require('./routes/auth.routes'));
+app.use(`${version_path}/search`, require('./routes/busqueda.routes'));
+app.use(`${version_path}/uploads`, require('./routes/uploads.routes'));
+app.use(`${version_path}/doctores`, require('./routes/doctor.routes'));
+app.use(`${version_path}/usuarios`, require('./routes/usuario.routes'));
+app.use(`${version_path}/hospitales`, require('./routes/hospital.routes'));
+
+// Servir aplicacion SPA
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'index.html'));
+});
 
 app.listen(port, () => {
     console.info(`Server is running on port ${port}`);
